e50767dd581f479026175c7ad09833322fa80843
Jake <deadh34d@gmail.com>
2015-10-02 12:59:13 -0400

    changed db connection credentials

--- model/database.class.php
+++ model/database.class.php
@@ -11,9 +11,9 @@
 
 class Database {
 
-    private static $dsn = 'mysql:host=mysql.designeroid.com;dbname=idk_custom;charset=utf8';
-    private static $username = 'idkdev';
-    private static $password = 'dev3I0^Ql9x56s5qekO2&u1YjKFrvEV';
+    private static $dsn = 'mysql:host=mysql.designeroid.com;dbname=rsihackathon;charset=utf8';
+    private static $username = 'rsihackathon';
+    private static $password = 'majormel37';
     private static $db;
 
     function __construct(){}
